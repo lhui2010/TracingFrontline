ce397baf66b9c4fd788182eede723d938c0d6932
Hui Liu <liuhui@vultr.guest>
2020-03-20 09:30:14 +0800

    Support one journal multiple rss feed

--- rss2table.py
+++ rss2table.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 import feedparser
+import sys
 import re
 import logging
 import time
-import argparse
-
-
-parser = argparse.ArgumentParser(description='A simple feed parser by formating rss links to table')
-parser.add_argument('rss_links', type=str, nargs = 1,
-                    help='A table containing journal name and rss links')
-parser.add_argument('--version', action='version', version='%(prog)s 0.1')
-args = parser.parse_args()
-
-
-
 
 # create logger
 logger = logging.getLogger('rss2html')
@@ -39,7 +29,7 @@ journal_list = []
 
 
 logger.info('Reading url list Complete...')
-with open(args.rss_links[0]) as file:
+with open(sys.argv[1]) as file:
     for line in file:
         if (len(line.strip())==0 or re.match("#", line)):
             continue
@@ -63,53 +53,59 @@ for j in journal_list:
     journal_count +=1
     #print (j)
     #print (journal_dict[j])
-    feed = feedparser.parse(journal_dict[j])
-    #print(feed)
+    for rss_url in journal_dict[j].rstrip().split(','):
+        feed = feedparser.parse(rss_url)
+        #print(feed)
 
-    feed_title = feed['feed']['title']
-    feed_entries = feed.entries
-#debug
-    #print (feed_entries)
+        feed_title = feed['feed']['title']
+        feed_entries = feed.entries
+    #debug
+        #print (feed_entries)
 
-    rss_link = {}
-    rss_time = {}
+        rss_link = {}
+        rss_time = {}
 
-    for entry in feed.entries:
-        article_title = entry.title
-        print_buf = ""
-#只输出基因组的
-#        if(not re.search(filter_keywords, article_title, re.IGNORECASE)):
-#            continue
-        try:
-            article_link = entry.link
-            article_published_at = re.sub(r'\d\d:\d\d:\d\d.*', '', entry.updated) # Unicode string
-            article_published_at_parsed = entry.updated_parsed # Time object
-            article_format_date = "{:04d}{:02d}{:02d}".format(entry.updated_parsed[0], entry.updated_parsed[1], entry.updated_parsed[2])
-            #article_author = entry.author + entry.authors
-            article_author = str(entry.author)
-            article_author_len = len(article_author)
-            if(article_author_len > 100):
-                article_author = article_author[0:50] + ".." + article_author[article_author_len-50:article_author_len]
-            if(len(entry.summary) >1):
-                article_summary = entry.summary
-            else:
-                article_summary = entry.description
-#leading infor like date and journal name for sorting
-            abstract_id = re.sub(r'[^\w]', '',article_title)
-            print_buf = "{}\t{}\t".format(sort_j_name, article_format_date)
-            print_buf += "<h5><b>[{}]</b> <a href={}>{}</a>. ".format(j, article_link, article_title)
-            print_buf += "{} {} ".format( article_author, article_published_at)
-            print_buf += '<button type="button" class="btn btn-primary btn-xs" data-toggle="collapse" data-target="#abstract{}">Abstract</button></h5><div id="abstract{}" class="collapse">{}</div>'.format(abstract_id, abstract_id,article_summary)
+        for entry in feed.entries:
+            article_title = entry.title
+            print_buf = ""
+    #只输出基因组的
+    #        if(not re.search(filter_keywords, article_title, re.IGNORECASE)):
+    #            continue
+            try:
+                article_link = entry.link
+                article_published_at = re.sub(r'\d\d:\d\d:\d\d.*', '', entry.updated) # Unicode string
+                article_published_at_parsed = entry.updated_parsed # Time object
+                article_format_date = "{:04d}{:02d}{:02d}".format(entry.updated_parsed[0], entry.updated_parsed[1], entry.updated_parsed[2])
+                #article_author = entry.author + entry.authors
+                article_author = str(entry.author)
+                article_author_len = len(article_author)
+                if(article_author_len > 100):
+                    article_author = article_author[0:50] + ".." + article_author[article_author_len-50:article_author_len]
+                if(len(entry.summary) >1):
+                    article_summary = entry.summary
+                else:
+                    article_summary = entry.description
+    #leading infor like date and journal name for sorting
+                abstract_id = re.sub(r'[^\w]', '',article_title)
+                print_buf = "{}\t{}\t".format(sort_j_name, article_format_date)
+                print_buf += "<h5><b>[{}]</b> <a href={}>{}</a>. ".format(j, article_link, article_title)
+                print_buf += "{} {} ".format( article_author, article_published_at)
+                print_buf += '<button type="button" class="btn btn-primary btn-xs" data-toggle="collapse" data-target="#abstract{}">Abstract</button></h5><div id="abstract{}" class="collapse">{}</div>'.format(abstract_id, abstract_id,article_summary)
 
-        except:
-            logger.error("Processing {} in  {} failed!".format(article_title, j))
-            continue
-        print_buf = re.sub(r'<img src.*?/>|\n', '', print_buf)
-        rss_link[article_link] = print_buf
-        rss_time[article_link] = article_published_at_parsed
+            except:
+                logger.error("Processing {} in  {} failed!".format(article_title, j))
+                continue
+            print_buf = re.sub(r'<img src.*?/>|\n', '', print_buf)
+            rss_link[article_link] = print_buf
+            rss_time[article_link] = article_published_at_parsed
 
-    for k in rss_link.keys():
-        print(rss_link[k])
+    #    printer=""
+        for k in rss_link.keys():
+            print(rss_link[k])
+
+    #        printer +=rss_link[k]+"\n"
+    #    printer = re.sub(r'\n$', '<hr>', printer)
+    #    print(printer)
 
 
 
